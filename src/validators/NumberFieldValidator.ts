6ea24001920879a622a08ddf485695563b56e548
alina kamatali <alinakamatali@gmail.com>
2025-12-03 13:37:16 +0300

    NumberFieldValidator in procces

---
+++ src/validators/NumberFieldValidator.ts
@@ -0,0 +1,87 @@
+import type { NumberFieldValidator } from '../types/validators/NumberFieldValidator';
+
+// типизация записи правил
+interface NumberValidationRule {
+  check: (value: number) => boolean;
+  message: string;
+}
+
+export class NumberFieldValidatorImpl implements NumberFieldValidator {
+  private rules: NumberValidationRule[] = [];
+
+  // будет использоваться позже
+  private fieldName: string;
+
+  constructor(fieldName: string) {
+    this.fieldName = fieldName;
+  }
+
+  //   методы
+  min(
+    val: number,
+    message = `Длина должна быть не менее ${val} символов`
+  ): this {
+    this.rules.push({
+      check: (value) => value >= val,
+      message,
+    });
+    return this;
+  }
+
+  max(
+    len: number,
+    message = `Длина должна быть не более ${len} символов`
+  ): this {
+    this.rules.push({
+      check: (value) => value.length <= len,
+      message,
+    });
+    return this;
+  }
+
+  email(message = 'Некорректный адрес электронной почты'): this {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    this.rules.push({
+      check: (value) => emailRegex.test(value),
+      message,
+    });
+    return this;
+  }
+
+  required(message = 'Поле обязательно для заполнения'): this {
+    this.rules.push({
+      check: (value) => value != null && value.trim() !== '',
+      message,
+    });
+    return this;
+  }
+
+  pattern(regex: RegExp, message = 'Значение не соответствует шаблону'): this {
+    this.rules.push({
+      check: (value) => regex.test(value),
+      message,
+    });
+    return this;
+  }
+
+  validate(value: unknown): { valid: boolean; error?: string } {
+    let stringValue: string;
+
+    // проверка введённого пользователем значения на строковость
+    if (value == null) {
+      stringValue = '';
+    } else if (typeof value === 'string') {
+      stringValue = value;
+    } else {
+      stringValue = String(value);
+    }
+
+    for (const rule of this.rules) {
+      if (!rule.check(stringValue)) {
+        return { valid: false, error: rule.message };
+      }
+    }
+
+    return { valid: true };
+  }
+}
