1dda62671a96f0de33180d1aa4c24dd262a75bc0
Katsero <raleksandrd25@gmail.com>
2025-12-08 14:21:38 +0300

    FieldValidator uncommented, done

--- src/FieldValidator.ts
+++ src/FieldValidator.ts
@@ -3,7 +3,7 @@
 // Импортируем реализации (классы), а не только типы
 import { StringFieldValidatorImpl } from "./validators/StringFieldValidator";
 import { NumberFieldValidatorImpl } from "./validators/NumberFieldValidator";
-// import { ArrayFieldValidatorImpl } from "./validators/ArrayFieldValidator"; //TODO разкомментить как будет готова логика ArrayFieldValidator
+import { ArrayFieldValidatorImpl } from "./validators/ArrayFieldValidator";
 
 // Импортируем интерфейс для соответствия контракту
 import type { FieldValidator } from "./types/FieldValidator";
@@ -12,26 +12,25 @@ import type { FieldValidator } from "./types/FieldValidator";
  * Реализация FieldValidator — фабрика для создания типизированных валидаторов.
  * Принимает имя поля, чтобы передать его внутрь конкретного валидатора (для отладки или будущих сообщений).
  */
-export class FieldValidatorImpl implements FieldValidator { //TODO ошибка ввиду отсутствия логики ArrayFieldValidator (ошибка типизации). Но в остальном вроде должно работать
+export class FieldValidatorImpl implements FieldValidator {
   private fieldName: string;
 
   constructor(fieldName: string) {
     this.fieldName = fieldName;
   }
 
-    // валидатор для строковых значений.
+  // валидатор для строковых значений.
   string(): StringFieldValidatorImpl {
     return new StringFieldValidatorImpl(this.fieldName);
   }
 
-    // валидатор для числовых значений.
+  // валидатор для числовых значений.
   number(): NumberFieldValidatorImpl {
     return new NumberFieldValidatorImpl(this.fieldName);
   }
 
-
-   // валидатор для массивов.
-//   array(): ArrayFieldValidatorImpl { //TODO разкомментить как будет готова логика ArrayFieldValidator
-//     return new ArrayFieldValidatorImpl(this.fieldName);
-//   }
-// }
+  // валидатор для массивов.
+  array(): ArrayFieldValidatorImpl {
+    return new ArrayFieldValidatorImpl(this.fieldName);
+  }
+}
